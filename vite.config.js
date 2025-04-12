d428835596fc5b540c9707f1f67fbbbe6c379f1f
umeryounus1994 <umeryounus92@gmail.com>
2025-04-13 01:00:06 +0500

    update vite.config.js
    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: parseInt(process.env.PORT) || 4173, // 🔑 Use DO's dynamic port
+    host: '0.0.0.0', // 🔑 Required so external traffic can reach the app
+  }
 })
